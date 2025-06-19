ba366f5a017a9226e209386e6eaa2e67dd351788
linsyking <kxiang@umich.edu>
2025-06-19 13:53:36 +0800

    fix: remove update

--- messengercli/messenger.py
+++ messengercli/messenger.py
@@ -11,6 +11,7 @@ from .updater import Updater
 
 app = typer.Typer(add_completion=False, help="Messenger CLI")
 API_VERSION = "1.2.0"
+CLI_VERSION = "0.5.3"
 
 SCENE_DIR = "src/Scenes"
 SCENEPROTO_DIR = "src/SceneProtos"
@@ -93,66 +94,7 @@ class Messenger:
         if self.config["auto_commit"]:
             execute_cmd("git add ./messenger.json")
 
-    def update_config(self):
-        self.config["version"] = API_VERSION
-        self.config["template_repo"] = {"url": "", "tag": ""}
-        if execute_cmd("git rev-parse --is-inside-work-tree", allow_err=True)[0] == 0 and self.config.get("auto_commit"):
-            self.config["auto_commit"] = True
-        else:
-            self.config["auto_commit"] = False
-        self.config["scenes"] = {}
-        self.config["sceneprotos"] = {}
-                    
-        os.chdir(".messenger")
-        if execute_cmd("git rev-parse --is-inside-work-tree", allow_err=True)[0] != 0:
-            print("No git repository found in .messenger directory.")
-        else:
-            urlcode, urlres = execute_cmd("git remote get-url origin", allow_err=True)
-            if urlcode == 0:
-                self.config["template_repo"]["url"] = urlres.strip()
-            else:
-                print("No remote repository found. Please set the remote repository URL in messenger.json manually.")
-            tagcode, tagres = execute_cmd("git describe --tags --exact-match", allow_err=True)
-            if tagcode == 0:
-                self.config["template_repo"]["tag"] = tagres.strip()
-            else:
-                branchcode, branchres = execute_cmd("git rev-parse --abbrev-ref @{u}", allow_err=True)
-                if branchcode == 0:
-                    if re.fullmatch(r'origin/(main|master)', branchres.strip()):
-                        self.config["template_repo"]["tag"] = "" 
-                    else:
-                        self.config["template_repo"]["tag"] = branchres.strip()[len('origin/'):]
-                else:
-                    print("No tag or branch found. Please set the tag or branch in messenger.json manually.")
-        os.chdir("..")
 
-        if os.path.exists(SCENEPROTO_DIR):
-            self.__update_scene(SCENEPROTO_DIR, True)
-        if not os.path.exists(SCENE_DIR):
-            os.mkdir(SCENE_DIR)
-        self.__update_scene(SCENE_DIR, False)
-        self.dump_config()
-
-    def __update_scene(self, sceneDir: str, isProto: bool):
-        field = "sceneprotos" if isProto else "scenes"
-        for sceneName in sorted(os.listdir(sceneDir), key=lambda d: os.path.join(sceneDir, d)):
-            if os.path.isdir(os.path.join(sceneDir, sceneName)):
-                scene = os.path.join(sceneDir, sceneName)
-                if not os.path.exists(os.path.join(scene, "Model.elm")):
-                    continue
-                self.config[field][sceneName] = {}
-                with open(os.path.join(scene, "Model.elm"), "r") as f:
-                    content = f.read()
-                    if not isProto and "LevelInit" in content:
-                        pattern = r'import SceneProtos\.(\w+)\.Model'
-                        match = re.search(pattern, content)
-                        if match:
-                            self.config[field][sceneName]["sceneproto"] = match.group(1)
-                            self.config["sceneprotos"][match.group(1)]["levels"].append(sceneName)
-                    self.config[field][sceneName]["raw"] = "import Messenger.Scene.RawScene" in content
-                if isProto:
-                    self.config[field][sceneName]["levels"] = []
-                    
     def add_level(self, name: str, sceneproto: str):
         """
         Add a level
@@ -831,20 +773,6 @@ def layer(
     print("Done!")
 
 
-@app.command()
-def update():
-    msg = Messenger()
-    input(
-        f"You are going to update messenger.json according to your project, continue?"
-    )
-    msg.update_config()
-    msg.format()
-    if msg.config["auto_commit"]:
-        execute_cmd(
-            "git commit -m 'build(Messenger): update messenger.json'"
-        )
-    print("Done!")
-
 
 @app.command()
 def remove(
@@ -892,7 +820,7 @@ def remove(
 @app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 def font(
     ctx : typer.Context,
-    range=typer.Option(4, "--range", help="Set the distance range."),
+    range : int =typer.Option(4, "--range", help="Set the distance range."),
 ):
     args = ctx.args
     # Check if the tool exists
@@ -1086,7 +1014,7 @@ def main(
     if ctx.invoked_subcommand is None and not version:
         typer.echo(ctx.get_help())
     if version:
-        print(f"Messenger API v{API_VERSION}")
+        print(f"Messenger CLI v{CLI_VERSION}, configuration file scheme v{API_VERSION}")
 
 
 
